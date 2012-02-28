8cceddfaab74651e8c8cd0e4216e74d635b446b6
GuoxiongXie <felixxie1218@gmail.com>
2012-02-27 19:04:16 -0800

    added some relationships between tables

--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,3 +1,4 @@
 class Calendar < ActiveRecord::Base
   validates :name, :presence => true #Makes sure all calendars have a name. DEBUG: unique???    
+  has_many :events  #DEBUG: events need s??
 end
--- app/models/users.rb
+++ app/models/users.rb
@@ -1,3 +1,4 @@
 class Users < ActiveRecord::Base
   validates :name, :presence => true, :uniqueness => true #Makes sure all useres have a unique name
+  has_many :calendars  #DEBUG: calendars need s??
 end
